a06e0ed7051f16cec054b4b90ac403ca4fea7db9
Amy Wichelow <i7683807@bournemouth.ac.uk>
2018-05-12 17:46:12 +0100

    hide previous button actually working

--- Camera Detection Prototype/BasicTutorial/StepViewController.swift
+++ Camera Detection Prototype/BasicTutorial/StepViewController.swift
@@ -18,10 +18,18 @@ class StepViewContoller: UIViewController {
     @IBOutlet weak var previousStepOutlet: UIButton!
     
     @IBAction func previousStepButton(_ sender: Any) {
+        
+        
     }
     
     @IBAction func nextStepButton(_ sender: Any) {
-        stepLabel.text = "Step \(tutorialSteps.count + 1)"
+        
+    //    stepLabel.text = "Step \(tutorialSteps.count + 1)"
+    
+    //if step label = tutorial step count + 1  then take user to share result screen
+    
+        
+        
     }
     
     @IBOutlet weak var tutorialStepDescription: UITextView!
@@ -31,6 +39,26 @@ class StepViewContoller: UIViewController {
     var tutorialSteps = [TutorialStep]()
     
     override func viewDidLoad() {
+        
+        stepLabel.text = "Step 1"
+        print(tutorial.tutorialName)
+        
+        title = tutorial.tutorialName
+        for set in tutorial.steps {
+            print(set.tutorialStepDescription)
+        }
+        
+        if stepLabel.text == "Step 1" {
+            
+            self.previousStepOutlet.isHidden = true
+            
+        }
+        
+        else {
+
+            self.previousStepOutlet.isHidden = false
+
+        }
         
         let tutorialRef = Database.database().reference().child("tutorials").child(tutorial.uuid!).child("steps")
         
@@ -45,25 +73,8 @@ class StepViewContoller: UIViewController {
                 }
             }
         })
-
-        
-        stepLabel.text = "Step 1"
-        print(tutorial.tutorialName)
-        
-        title = tutorial.tutorialName
-        for set in tutorial.steps {
-            print(set.tutorialStepDescription)
-        }
-        
-        if stepLabel.text == "Step 1" {
-            
-            self.previousStepOutlet.isHidden = true
-            
-        } else {
-            
-            self.previousStepOutlet.isHidden = false
-            
-        }
+    
+    
         
     }
     
